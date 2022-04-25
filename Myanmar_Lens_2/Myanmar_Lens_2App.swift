70eef48d0b955d3adb3b53394560f764418970bf
Aung Ko Min <jonahaung@gmail.com>
2022-04-25 09:52:34 +0800

    Initial Commit

---
+++ Myanmar_Lens_2/Myanmar_Lens_2App.swift
@@ -0,0 +1,20 @@
+//
+//  Myanmar_Lens_2App.swift
+//  Myanmar_Lens_2
+//
+//  Created by Aung Ko Min on 25/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Myanmar_Lens_2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
